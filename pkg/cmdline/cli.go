58f60f5ded170bb21ebd04d8f3ec80f6ef63317e
Sonu Jose <sonujose993@gmail.com>
2022-12-07 11:21:53 +0530

    Updating the core staructure and commands

--- pkg/cmdline/cli.go
+++ pkg/cmdline/cli.go
@@ -1,35 +1,77 @@
 package cmdline
 
 import (
-	"fmt"
-
+	"github.com/sonujose/sloop/pkg/core/template"
 	"github.com/sonujose/sloop/pkg/logger"
-	"github.com/sonujose/sloop/pkg/template"
 	"github.com/urfave/cli/v2"
 )
 
 func Initialize() *cli.App {
 	app := &cli.App{
-		Name:  "sloop",
-		Usage: "Sloop package Manager",
-		Action: func(*cli.Context) error {
-			fmt.Println("Welcome!!! Sloop package manager is ready...")
-			return nil
-		},
+		Name:                 "sloop",
+		Usage:                "A simple Kubernetes Package manager for all lazy folks",
+		EnableBashCompletion: true,
 		Commands: []*cli.Command{
 			{
 				Name:    "template",
-				Usage:   "Render the sloop packages as templates to the output source",
-				Aliases: []string{"t", "tpl"},
+				Usage:   "locally Render the sloop package configurations",
+				Aliases: []string{"t"},
 				Flags: []cli.Flag{
 					&cli.StringFlag{
 						Name:    "output",
-						Aliases: []string{"o", "out"},
+						Aliases: []string{"o"},
 						Usage:   "specify the standard output for templating",
 						EnvVars: []string{"OUTPUT"},
 					},
 				},
 				Action: sloopTemplateAction,
+			},
+			{
+				Name:    "controller",
+				Usage:   "manage sloop controller",
+				Aliases: []string{"ctrl"},
+				Subcommands: []*cli.Command{
+					{
+						Name:  "install",
+						Usage: "install the sloop controller in the cluster, if not present",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:     "namespace",
+								Aliases:  []string{"n"},
+								Usage:    "Specify the namespace for controller installation",
+								Required: true,
+							},
+						},
+					},
+					{
+						Name:  "status",
+						Usage: "check the status of the sloop controller in the cluster",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:     "namespace",
+								Aliases:  []string{"n"},
+								Usage:    "Specify the namespace for controller installation",
+								Required: true,
+							},
+						},
+					},
+				},
+			},
+			{
+				Name:  "apply",
+				Usage: "apply the slooper configurations to the target cluster",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:  "dry-run",
+						Value: false,
+						Usage: "Do a dry run for the installation, checks all configurations",
+					},
+					&cli.StringFlag{
+						Name:    "namespace",
+						Aliases: []string{"n"},
+						Usage:   "Specify the namespace for the target installation",
+					},
+				},
 			},
 		},
 	}
---
+++ pkg/core/apply/apply.go
@@ -0,0 +1,5 @@
+package apply
+
+func applyPackage() {
+
+}
